c5d8003d29663dded153f657a448951cc16c8efc
iamlive <71422116+iamcrypto@users.noreply.github.com>
2025-01-28 09:21:00 +0530

    add comit
    noe noidejs

---
+++ index.js
@@ -0,0 +1,99 @@
+
+
+const express = require('express');
+const http = require('http');
+
+var admin = require("firebase-admin");
+const axios = require("axios")
+
+const CURRENT_VERSION = 1;
+
+const piNetworkApi = 'socialchain.app/v2'
+const API_KEY = 't3rfmdht1odwawijsh0wwdozozqaislivnf4pioutezhj5rgbl2beymdehm4o5pc'
+
+const port = process.env.PORT || 3005
+
+var app = express();
+const server = http.createServer(app)
+
+
+
+var serviceAccount = require("C:/Users/HOME/Documents/GitHub/firebasechat/nodejs.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://live-chess-dede7-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+app.use(express.json());
+app.use(express.static(__dirname + "/"));
+
+const database = admin.database();
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+ 
+
+  
+const config = {headers:{'Authorization': 'Key '}}
+  app.post('/approve', async (req, res) => {
+    const { paymentId } = req.body
+    try {
+        //send /approve POST request
+        await axios.post(`https://${piNetworkApi}/payments/${paymentId}/approve`, {}, {
+          headers: {
+            'Authorization': `Key ${API_KEY}`
+          }
+        })
+    
+        return res.status(200).send({
+          message: 'Payment approved!',
+          status: 'success'
+        });
+      } catch (err) {
+        //output error for debugging
+        console.log(err)
+          
+        //return something to your front end
+        return res.status(500).send({
+          message: `There has been an error!`,
+          status: 'error'
+        })
+      }
+  });
+  app.post('/complete', async(req, res) => {
+    const { paymentId, txid } = req.body
+    try {
+      //send /approve POST request
+      await axios.post(`https://${piNetworkApi}/payments/${paymentId}/complete`, {
+        txid
+      }, {
+        headers: {
+          'Authorization': `Key ${API_KEY}`
+        }
+      })
+  
+      return res.status(200).send({
+        message: 'Payment completed!',
+        status: 'success'
+      });
+    } catch (err) {
+      //output error for debugging
+      console.log(err)
+        
+      //return something to your front end
+      return res.status(500).send({
+        message: `There has been an error!`,
+        status: 'error'
+      })
+    }
+  });
+
+
+server.listen(port);
+console.log("http://localhost:" + port);
