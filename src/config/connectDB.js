917c69792c848e189cf71b411f3436b29f27112c
iamlive <71422116+iamcrypto@users.noreply.github.com>
2025-02-21 14:07:34 +0530

    commit changes
    ok

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,15 +1,7 @@
 const mysql = require('mysql2/promise');
 
 import "dotenv/config";
-/* 
-const connection = mysql.createPool({
-  host: 'dbtiran.cjakccac2nys.ap-south-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Ajax24x7#365',
-  database: 'win',
-  port: '3306' 
-   
-}); */
+
 
 const connection = mysql.createPool({
   host: process.env.DATABASE_HOST,
