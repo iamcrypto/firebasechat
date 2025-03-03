cb79bc44345f5d633f90ff9b6cc2b873b4227665
Swapnayg <86843363+Swapnayg@users.noreply.github.com>
2025-03-03 10:44:41 +0530

    updated with withdrawal logic

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1101,18 +1101,43 @@ const infoUserBank = async (req, res) => {
     let checkTime = timerJoin(date);
     const [recharge] = await connection.query('SELECT * FROM recharge WHERE phone = ? AND status = 1', [userInfo.phone]);
     const [minutes_1] = await connection.query('SELECT * FROM minutes_1 WHERE phone = ?', [userInfo.phone]);
+    const [d5_minute] = await connection.query('SELECT * FROM result_5d WHERE phone = ?', [userInfo.phone]);
+    const [k3_minute] = await connection.query('SELECT * FROM result_k3 WHERE phone = ?', [userInfo.phone]);
+    const [trx_minute] = await connection.query('SELECT * FROM trx_wingo_bets WHERE phone = ?', [userInfo.phone]);
     let total = 0;
+
     recharge.forEach((data) => {
         total += parseFloat(data.money);
     });
+
     let total2 = 0;
-    minutes_1.forEach((data) => {
-        total2 += parseFloat(data.money);
-    });
+    let total_w = 0;
+    let total_k3 = 0;
+    let total_5d = 0;
+    let total_trx = 0;
+    let total_w_fee = 0;
+    let total_k3_fee = 0;
+    let total_5d_fee = 0;
+    let total_trx_fee = 0;
     let fee = 0;
     minutes_1.forEach((data) => {
-        fee += parseFloat(data.fee);
+        total_w += parseFloat(data.money);
+        total_w_fee += parseFloat(data.fee);
     });
+    k3_minute.forEach((data) => {
+        total_k3 += parseFloat(data.money);
+        total_k3_fee += parseFloat(data.fee);
+    });
+    d5_minute.forEach((data) => {
+        total_5d += parseFloat(data.money);
+        total_5d_fee += parseFloat(data.fee);
+    });
+    trx_minute.forEach((data) => {
+        total_trx += parseFloat(data.money);
+        total_trx_fee += parseFloat(data.fee);
+    });
+    total2 += parseInt(total_w) + parseInt(total_k3) + parseInt(total_5d) + parseInt(total_trx);
+    fee += parseInt(total_w_fee) + parseInt(total_k3_fee) + parseInt(total_5d_fee) + parseInt(total_trx_fee);
 
     result = Math.max(result, 0);
     let result = 0;
